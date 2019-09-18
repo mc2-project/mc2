ad9fb12b21e4ee5554ebdcd5ade1ee339fc13c47
chester-leung <root@ip-172-31-41-140.us-west-2.compute.internal>
2019-09-18 08:49:33 +0000

    remove old directory

--- federated-xgboost/camp/xgb_test.py
+++
@@ -1,7 +0,0 @@
-from FederatedXGBoost import FederatedXGBoost
-
-fxgb = FederatedXGBoost()
-fxgb.load_test_data('/home/ubuntu/data/msd_test_data_split.csv')
-fxgb.load_model(/home/ubuntu/data/msd_test_data_split.csv)
-fxgb.eval()
-fxgb.shutdown()
--- federated-xgboost/camp/xgb_train.py
+++
@@ -1,9 +0,0 @@
-# %load xgb_train.py
-
-from FederatedXGBoost import FederatedXGBoost
-
-fxgb = FederatedXGBoost()
-fxgb.load_training_data('/home/ubuntu/data/msd_training_data_split.csv')
-fxgb.train({'max_depth': 3, 'min_child_weight': 0.0, 'lambda': 1.0}, 40)
-fxgb.save_model('jn_model.model')
-fxgb.shutdown()
