0b8d86d3ef956926bfec13ac91b1b66c9bfe597d
Chester Leung <chester.leung@berkeley.edu>
2021-04-02 17:21:47 -0700

    Modifications for Docker quickstart (#117)
    * xgb demo changes
    
    * Modifications

--- demo/opaque_xgboost_demo.py
+++ demo/opaque_xgboost_demo.py
@@ -2,14 +2,14 @@ import mc2client.xgb as xgb
 
 # Load training data
 # TODO: fill in the path to your training data
-dtrain = xgb.DMatrix({"user1": "/home/mc2/data/data.txt.train.enc?format=csv&label_column=9"})
+dtrain = xgb.DMatrix({"user1": "/root/data/opaquexgb.csv.train.enc?format=csv&label_column=9"})
 
 # Get number of columns in training data
 training_num_cols = dtrain.num_col()
 
 # Load test data
 # TODO: fill in the path to your test data
-dtest = xgb.DMatrix({"user1": "/home/mc2/data/data.txt.test.enc?format=csv&label_column=9"})
+dtest = xgb.DMatrix({"user1": "/root/data/opaquexgb.csv.test.enc?format=csv&label_column=9"})
 
 # Get number of columns in test data
 test_num_col = dtest.num_col()
@@ -31,6 +31,9 @@ bst = xgb.Booster(params, [dtrain])
 num_rounds = 10
 for i in range(num_rounds):
     bst.update(dtrain, i, None)
+
+# Save model
+bst.save_model("/root/results/xgb.model")
 
 # Get fscores of model
 feature_map = bst.get_fscore()
