47e70e60a203225803a3c37a757dd839ad7d28fc
Pan Chasinga <jo.chasinga@gmail.com>
2021-08-18 06:00:41 -0700

    Add log to test

--- python-package/tests/azure/delete_azure_resources.py
+++ python-package/tests/azure/delete_azure_resources.py
@@ -2,30 +2,32 @@ import os
 import pathlib
 
 import mc2client as mc2
+from . import test_logger as logger
 
-print("Setting config file path")
+
+logger.info("Setting config file path")
 tests_dir = pathlib.Path(__file__).parent.absolute()
 config = os.path.join(tests_dir, "../test.yaml")
 mc2.set_config(config)
 
 download_path = os.path.join(tests_dir, "dummy_downloaded.txt")
 
-print("Downloading file")
+logger.info("Downloading file")
 mc2.download_file("dummy.txt", download_path)
 
 if not os.path.isfile(download_path):
-    print("Error: Couldn't download file from Azure")
+    logger.info("Error: Couldn't download file from Azure")
 else:
     os.remove(download_path)
 
-print("Deleting container")
+logger.info("Deleting container")
 mc2.delete_container()
 
-print("Deleting storage")
+logger.info("Deleting storage")
 mc2.delete_storage()
 
-print("Deleting cluster")
+logger.info("Deleting cluster")
 mc2.delete_cluster()
 
-print("Deleting resource group")
+logger.info("Deleting resource group")
 mc2.delete_resource_group()
