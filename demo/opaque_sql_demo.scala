fbedc8dc98218810acefba67f26df8c9a5da198b
Octavian Sima <octavian.sima98@gmail.com>
2021-09-21 19:57:51 +0000

    update link in docs, standarize the format across the demo scripts and remove 3 extra spaces at end, remove opaque-systems in docs

--- demo/opaque_sql_demo.scala
+++ demo/opaque_sql_demo.scala
@@ -2,8 +2,8 @@ import edu.berkeley.cs.rise.opaque.implicits._
 import org.apache.spark.sql.types._
 
 val df = spark.read.format("edu.berkeley.cs.rise.opaque.EncryptedSource").load("/root/data/opaquesql.csv.enc")
+
 val result = df.filter($"Age" < lit(30))
+
 // This will save the result DataFrame to the result directory on the cloud
 result.write.format("edu.berkeley.cs.rise.opaque.EncryptedSource").save("/root/results/opaque_sql_result")
-
-
